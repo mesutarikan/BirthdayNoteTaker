161035763080abd6d91e2cc77094a2a9bc60be81
mesutarikan <mesutarikan4344@gmail.com>
2021-10-14 14:34:52 +0300

    first commit

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String{
+            nameLabel.text = newName
+        }
+        if let newBirthday = storedBirthday as? String{
+            birthdayLabel.text = newBirthday
+        }
+        
     }
 
-
+    @IBAction func deleteButton(_ sender: Any) {
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if (storedName  as? String) != nil{
+            UserDefaults.standard.removeObject(forKey: "name")
+        }
+        if (storedBirthday as? String) != nil{
+            UserDefaults.standard.removeObject(forKey: "birthday")
+        }
+    }
+    @IBAction func saveButton(_ sender: Any) {
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "BirthDay: \(birthdayTextField.text!)"
+    }
+    
 }
 
